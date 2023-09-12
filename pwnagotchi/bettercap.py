fe34c1e76d54d07c10135cc48ccd4c1c5515cd36
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2023-09-12 13:17:37 +0200

    Version 2.3.8
    Signed-off-by: Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>

--- pwnagotchi/bettercap.py
+++ pwnagotchi/bettercap.py
@@ -40,29 +40,26 @@ class Client(object):
 
     async def start_websocket(self, consumer):
         s = "%s/events" % self.websocket
-        async with websockets.connect(s, ping_interval=60, ping_timeout=90) as ws:
-            # After the websocket connection is opened, loop waiting for messages
-            # Moved the websocket connection before the loop to avoid runaway websocket
-            # connections that were exhausting all possible TCP ports (ephemeral port exhaustion)
-            while True:
-                try:
+        while True:
+            try:
+                async with websockets.connect(s, ping_interval=60, ping_timeout=90) as ws:
                     async for msg in ws:
                         try:
                             await consumer(msg)
                         except Exception as ex:
                                 logging.debug("Error while parsing event (%s)", ex)
-                except websockets.ConnectionClosedError:
-                    logging.error("Lost websocket connection. Reconnecting...")
-                    continue
-                except websockets.WebSocketException as wex:
-                    logging.error("Websocket exception (%s)", wex)
-                    continue
-                except OSError as e:
-                    logging.error("Websocket OSError exception (%s) with parameter %s", e, s)
-                    continue
-                except Exception as e:
-                    logging.error("Other exception (%s) with parameter %s", e, s)
-                    continue
+            except websockets.ConnectionClosedError:
+                logging.error("Lost websocket connection. Reconnecting...")
+                continue
+            except websockets.WebSocketException as wex:
+                logging.error("Websocket exception (%s)", wex)
+                continue
+            except OSError as e:
+                logging.error("Websocket OSError exception (%s) with parameter %s", e, s)
+                continue
+            except Exception as e:
+                logging.error("Other exception (%s) with parameter %s", e, s)
+                continue
 
     def run(self, command, verbose_errors=True):
         for _ in range(0,2):
