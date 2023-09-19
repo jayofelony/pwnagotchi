98d3f519e44b3e672e99743f19350e863329fe11
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2023-09-19 19:55:09 +0200

    internet-connection.py: change position of the label
    Signed-off-by: Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>

--- pwnagotchi/plugins/default/internet-connection.py
+++ pwnagotchi/plugins/default/internet-connection.py
@@ -32,7 +32,7 @@ class InternetConnectionPlugin(plugins.Plugin):
             # add a LabeledValue element to the UI with the given label and value
             # the position and font can also be specified
             ui.add_element('connection_status', components.LabeledValue(color=view.BLACK, label='WWW', value='-',
-                                                                        position=(ui.width() / 2 - 30, 0),
+                                                                        position=(ui.width() / 2 - 40, 0),
                                                                         label_font=fonts.Bold, text_font=fonts.Medium))
 
     def on_ui_update(self, ui):
