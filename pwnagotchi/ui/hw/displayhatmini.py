f3081db363c6beaf69324b38857502b7f534957a
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2023-10-02 09:07:44 +0200

    v2.4.5
    Signed-off-by: Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>

--- pwnagotchi/ui/hw/displayhatmini.py
+++ pwnagotchi/ui/hw/displayhatmini.py
@@ -10,20 +10,22 @@ class DisplayHatMini(DisplayImpl):
         self._display = None
 
     def layout(self):
-        fonts.setup(12, 10, 12, 55, 25, 9)
+        fonts.setup(12, 10, 12, 70, 25, 9)
         self._layout['width'] = 320
         self._layout['height'] = 240
-        self._layout['face'] = (14, 28)
-        self._layout['name'] = (28, 20)
-        self._layout['channel'] = (278, 96)
-        self._layout['aps'] = (224, 96)
-        self._layout['uptime'] = (134, 3)
+        self._layout['face'] = (35, 50)
+        self._layout['name'] = (5, 20)
+        self._layout['channel'] = (0, 0)
+        self._layout['aps'] = (40, 0)
+        self._layout['uptime'] = (240, 0)
+        self._layout['line1'] = [0, 14, 320, 14]
+        self._layout['line2'] = [0, 220, 320, 220]
         self._layout['friend_face'] = (0, 130)
         self._layout['friend_name'] = (40, 135)
-        self._layout['shakes'] = (34, 193)
-        self._layout['mode'] = (22, 3)
+        self._layout['shakes'] = (0, 220)
+        self._layout['mode'] = (280, 220)
         self._layout['status'] = {
-            'pos': (26, 124),
+            'pos': (80, 160),
             'font': fonts.status_font(fonts.Medium),
             'max': 20
         }
