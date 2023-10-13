c3e5b3ea716dcaae43dd2b4fc1b64c10c47c97d6
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2023-10-13 15:46:48 +0200

    v2.4.7
    Signed-off-by: Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>

--- pwnagotchi/plugins/default/gdrivesync.py
+++ pwnagotchi/plugins/default/gdrivesync.py
@@ -111,8 +111,11 @@ class GdriveSync(plugins.Plugin):
             self.ready = False
 
     def get_folder_id_by_name(self, drive, folder_name):
-        file_list = drive.ListFile({'q': "name="+folder_name+" and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+        query = f"name='{folder_name}' and mimeType='application/vnd.google-apps.folder' and trashed=false"
+        file_list = drive.ListFile({'q': query}).GetList()
+        print(f"Query: {query}")
         for file in file_list:
+            print(f"File Title: {file['title']}, Expected Folder Name: {folder_name}")
             if file['title'] == folder_name:
                 return file['id']
         return None
