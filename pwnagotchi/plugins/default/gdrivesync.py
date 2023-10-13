d24a6cd4e472c9a3daae5e83bbce6d77b53fdb1b
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2023-10-13 14:53:58 +0200

    v2.4.7
    Signed-off-by: Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>

--- pwnagotchi/plugins/default/gdrivesync.py
+++ pwnagotchi/plugins/default/gdrivesync.py
@@ -69,12 +69,13 @@ class GdriveSync(plugins.Plugin):
 
             # if backup file does not exist, we will check for backup folder on gdrive.
             if not self.backup:
+                logging.info("test1")
                 # Use self.options['backup_folder'] as the folder ID where backups are stored
                 backup_folder_name = self.options['backup_folder']
                 backup_folder_id = self.get_folder_id_by_name(self.drive, backup_folder_name)
+                logging.info("test1.1")
 
                 if not backup_folder_id:
-                    logging.info("test")
                     # If the folder doesn't exist, create it
                     folder = self.drive.CreateFile(
                         {'title': backup_folder_name, 'mimeType': 'application/vnd.google-apps.folder'})
