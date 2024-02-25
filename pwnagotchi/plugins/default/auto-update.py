73e0a1fce7ae58a9962ecf6a207e6fdf960cca35
jayofelony <oudshoorn.jeroen@gmail.com>
2024-02-25 13:42:12 +0100

    Update auto-update.py

--- pwnagotchi/plugins/default/auto-update.py
+++ pwnagotchi/plugins/default/auto-update.py
@@ -189,7 +189,7 @@ class AutoUpdate(plugins.Plugin):
                 to_check = [
                     ('jayofelony/bettercap', parse_version('bettercap -version'), True, 'bettercap'),
                     ('jayofelony/pwngrid', parse_version('pwngrid -version'), True, 'pwngrid-peer'),
-                    ('jayofelony/pwnagotchi', pwnagotchi.__version__, False, 'pwnagotchi')
+                    ('jayofelony/bananagotchi', pwnagotchi.__version__, False, 'pwnagotchi')
                 ]
 
                 for repo, local_version, is_native, svc_name in to_check:
