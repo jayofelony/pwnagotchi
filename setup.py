7e4d926b14b501be4e6eeb97719851cf2f1dfb1f
jayofelony <oudshoorn.jeroen@gmail.com>
2024-03-01 10:07:33 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ setup(name='pwnagotchi',
           "install": CustomInstall,
       },
       scripts=['bin/pwnagotchi'],
-      package_data={'pwnagotchi': ['defaults.yml', 'pwnagotchi/defaults.yml', 'locale/*/LC_MESSAGES/*.mo']},
+      package_data={'pwnagotchi': ['defaults.toml', 'pwnagotchi/defaults.toml', 'locale/*/LC_MESSAGES/*.mo']},
       include_package_data=True,
       packages=find_packages(),
       classifiers=[
