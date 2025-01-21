948fe89ce6b5c09003b0c841a5b9507b79f29064
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2025-01-21 14:37:02 +0100

    Another fix for bt-tether
    Signed-off-by: Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>

--- pwnagotchi/plugins/default/bt-tether.py
+++ pwnagotchi/plugins/default/bt-tether.py
@@ -41,8 +41,8 @@ class BTTether(plugins.Plugin):
                 'bluetooth.type', 'panu',
                 'bluetooth.bdaddr', f'{mac}',
                 'ipv4.method', 'manual',
-                'ipv4.dns', '8.8.8.8;1.1.1.1;',
-                'ipv4.addresses', f'{address}',
+                'ipv4.dns', '8.8.8.8,1.1.1.1;',
+                'ipv4.addresses', f'{address}/24',
                 'ipv4.gateway', f'{gateway}',
                 'ipv4.route-metric', '100'
             ], check=True)
