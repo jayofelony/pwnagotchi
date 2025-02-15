e4df23c781b3f3063c4d6fae3d227eeea5b483e3
Johannes Häusler <mail@spotlightforbugs.eu>
2025-02-15 21:01:36 +0100

    Refactor Bluetooth setup and improve configuration wizard

--- pwnagotchi/automata.py
+++ pwnagotchi/automata.py
@@ -18,9 +18,9 @@ class Automata(object):
         self._view.on_miss(who)
 
     def _on_error(self, who, e):
-        # when we're trying to associate or deauth something that is not in range anymore
+        # when we're trying to associate or deauth something not in range anymore
         # (if we are moving), we get the following error from bettercap:
-        # error 400: 50:c7:bf:2e:d3:37 is an unknown BSSID or it is in the association skip list.
+        # error 400: 50:c7:bf:2e:d3:37 is an unknown BSSID, or it is in the association skip list.
         if 'is an unknown BSSID' in str(e):
             self._on_miss(who)
         else:
