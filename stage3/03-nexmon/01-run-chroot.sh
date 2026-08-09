9d162ad2fc079d32c13558681d26ae8ca7b62f38
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2026-08-09 13:48:04 +0200

    Small refactor for nexmon

--- stage3/03-nexmon/01-run-chroot.sh
+++ stage3/03-nexmon/01-run-chroot.sh
@@ -2,6 +2,6 @@
 
 cd /home/pi
 wget https://kali.download/kali/pool/non-free-firmware/f/firmware-nexmon/firmware-nexmon_0.2_all.deb
-wget https://http.kali.org/kali/pool/contrib/b/brcmfmac-nexmon-dkms/brcmfmac-nexmon-dkms_6.12.2_all.deb
+wget https://github.com/jayofelony/brcmfmac-nexmon-dkms/releases/download/v6.12.2%2Bndevfix1/brcmfmac-nexmon-dkms_6.12.2%2Bndevfix1_all.deb
 
 apt-get remove -y firmware-brcm80211
