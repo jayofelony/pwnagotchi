65839ee73857577946d42010186877f8ba28d9b1
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2026-08-09 17:11:44 +0200

    install nexmon during build

--- stage3/03-nexmon/01-run-chroot.sh
+++ stage3/03-nexmon/01-run-chroot.sh
@@ -2,6 +2,9 @@
 
 cd /home/pi
 wget https://kali.download/kali/pool/non-free-firmware/f/firmware-nexmon/firmware-nexmon_0.2_all.deb
-wget https://github.com/jayofelony/brcmfmac-nexmon-dkms/releases/download/v6.12.2%2Bndevfix1/brcmfmac-nexmon-dkms_6.12.2%2Bndevfix1_all.deb
+wget https://github.com/jayofelony/brcmfmac-nexmon-dkms/releases/download/v6.12.2%2Bndevfix3/brcmfmac-nexmon-dkms_6.12.2%2Bndevfix3_all.deb
+
+dpkg -i firmware-nexmon_0.2_all.deb brcmfmac-nexmon-dkms_6.12.2+ndevfix3_all.deb
+rm -r firmware-nexmon_0.2_all.deb brcmfmac-nexmon-dkms_6.12.2+ndevfix3_all.deb
 
 apt-get remove -y firmware-brcm80211
