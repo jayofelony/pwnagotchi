18c433109bf2cab5b436b67aa29e2f026d87ce03
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2026-08-28 10:22:04 +0200

    Change directory naming

--- stage3/00-pre-pwn/00-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-# Edit this file to make changes to the WORK_DIR prior to installing all Pwnagotchi packages.
-# So you don't have to remove the entire WORK_DIR
-# remove these commands after it ran
--- stage3/01-pwn-packages/01-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-install -v -m 0644 files/rpi-usb-gadget_1.0.6_arm64.deb "${ROOTFS_DIR}/home/pi/rpi-usb-gadget_1.0.6_arm64.deb"
