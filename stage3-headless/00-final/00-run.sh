8ecbd1b28d09e075443583661d32333f5e8397cf
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2026-08-28 14:09:33 +0200

    Install new rpi-usb-gadget package

--- stage3-headless/00-final/00-run.sh
+++ stage3-headless/00-final/00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 echo -e "\e[32m### Installing patched files ###\e[0m"
-install -v -m 0644 files/rpi-usb-gadget_1.0.6_arm64.deb "${ROOTFS_DIR}/home/pi/rpi-usb-gadget_1.0.6_arm64.deb"
+install -v -m 0644 files/rpi-usb-gadget_1.0.7_arm64.deb "${ROOTFS_DIR}/home/pi/rpi-usb-gadget_1.0.7_arm64.deb"
 install -v -m 755 files/user-data "${ROOTFS_DIR}/boot/firmware/user-data"
 install -v -m 755 files/config.txt "${ROOTFS_DIR}/boot/firmware/config.txt"
--- stage3/01-pwn-packages/01-run.sh
+++ stage3/01-pwn-packages/01-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-install -v -m 0644 files/rpi-usb-gadget_1.0.6_arm64.deb "${ROOTFS_DIR}/home/pi/rpi-usb-gadget_1.0.6_arm64.deb"
+install -v -m 0644 files/rpi-usb-gadget_1.0.7_arm64.deb "${ROOTFS_DIR}/home/pi/rpi-usb-gadget_1.0.7_arm64.deb"
